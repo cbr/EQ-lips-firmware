59c75ac6db704425b12879906ee7293fb2769ce2
Cedric Bregardis <cedric.bregardis@gmail.com>
2009-12-24 17:47:33 +0100

    Show button selection on screen

--- src/common/menu.asm
+++ src/common/menu.asm
@@ -8,12 +8,15 @@
 #include <global.inc>
 #include <menu.inc>
 #include <encoder.inc>
+#include <delay.inc>
 
 #define MENU_EQ_BAND_WIDTH          0x04
 #define MENU_EQ_BAND_FOCUS_WIDTH    0x05
 
 #define MENU_EQ_ZERO_VALUE          0x10
 #define MENU_EQ_VALUE_TO_LCD_SHT    0x00
+
+#define MENU_BUTTON_NB_DRAW_SELECT  0x8
 
     UDATA
 menu_eq_last_value         RES 1
@@ -237,5 +240,93 @@ menu_eq_manage_selection_quit:
 
     return
 
+;;; param1: y position of button
+menu_button_select_func:
+    global menu_button_select_func
 
+#if 1
+    ;; rectanle for the selected line only
+
+    ;; draw the focus rectangle twice with XOR
+    movlw MENU_BUTTON_NB_DRAW_SELECT
+    banksel menu_var1
+    movwf menu_var1
+menu_button_select_func_draw:
+    movf param1, W
+    movwf param2
+    lshift_f param2, LCD_CHAR_HEIGH_SHIFT
+
+    clrf param1
+    movlw LCD_WIDTH
+    movwf param3
+    movf param2, W
+    movwf param4
+    movlw LCD_CHAR_HEIGH
+    movwf param4
+    bsf param5, LCD_XOR
+    call_other_page lcd_rectangle
+    banksel menu_var1
+    decfsz menu_var1
+    goto menu_button_select_func_draw
+#endif
+#if 0
+    ;; rectangle for the entire screen
+
+    ;; draw the focus rectangle twice with XOR
+    movlw MENU_BUTTON_NB_DRAW_SELECT
+    banksel menu_var1
+    movwf menu_var1
+
+menu_button_select_func_draw:
+    clrf param1
+    clrf param2
+    movlw LCD_WIDTH/2
+    movwf param3
+    movlw LCD_HEIGH
+    movwf param4
+    bsf param5, LCD_XOR
+    call_other_page lcd_rectangle
+
+    movlw LCD_WIDTH/2
+    movwf param1
+    clrf param2
+    movlw LCD_WIDTH/2
+    movwf param3
+    movlw LCD_HEIGH
+    movwf param4
+    bsf param5, LCD_XOR
+    call_other_page lcd_rectangle
+    banksel menu_var1
+    decfsz menu_var1
+    goto menu_button_select_func_draw
+#endif
+
+#if 0
+    ;; scroll screen
+    ;; too fast for now ! try again when a real delay will be available
+
+    clrf menu_var2
+    movlw 32
+    movwf menu_var1
+menu_button_select_func_draw:
+    movlw LCD_DISPLAY_START_LINE
+    movwf param1
+    movf menu_var2, W
+    iorwf param1, F
+    bsf param2, LCD_COMMAND
+    bsf param2, LCD_FIRST_CHIP
+    call lcd_write
+
+    ;; delay
+    movlw 0xFF
+    call delay_wait
+
+    incf menu_var2, F
+    decfsz menu_var1, F
+    goto menu_button_select_func_draw
+
+#endif
+    lcd_send_cmd_1 LCD_DISPLAY_START_LINE, 0
+
+    return
 END
