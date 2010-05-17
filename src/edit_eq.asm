274eacaba6e754aa592366c8da148c3200643e29
Cedric Bregardis <cedric.bregardis@gmail.com>
2010-05-17 19:27:32 +0200

    Id for menu entries

--- src/edit_eq.asm
+++ src/edit_eq.asm
@@ -31,6 +31,9 @@ edit_eq_st_trem:
 button_sleep_st:
     dt "SLEEP", 0
 
+tst_select_spec:
+    menu_select_specific_entry 112
+    return
 
 edit_eq_show:
     global edit_eq_show
@@ -51,19 +54,21 @@ edit_eq_show:
 #ifdef TREMOLO
     menu_button_goto edit_eq_st_trem, 0, edit_trem_show
 #endif
-    menu_edit edit_common_st_bank, 1, 1, 1, BANK_NB, current_bank, edit_common_load, UNUSED_PARAM
-    menu_edit_no_show edit_common_st_save, 1, 2, 1, BANK_NB, current_bank, edit_common_refresh, edit_common_save
-    menu_eq (0x5*0 + 0x3D), bank_numpot_values, process_change_conf
-    menu_eq (0x5*1 + 0x3D), bank_numpot_values+1, process_change_conf
-    menu_eq (0x5*2 + 0x3D), bank_numpot_values+2, process_change_conf
-    menu_eq (0x5*3 + 0x3D), bank_numpot_values+3, process_change_conf
-    menu_eq (0x5*4 + 0x3D), bank_numpot_values+4, process_change_conf
-    menu_eq (0x5*5 + 0x3D), bank_numpot_values+5, process_change_conf
-    menu_eq (0x5*6 + 0x3D), bank_numpot_values+6, process_change_conf
-    menu_eq (0x5*7 + 0x3D), bank_numpot_values+7, process_change_conf
-    menu_eq (0x5*8 + 0x3D), bank_numpot_values+8, process_change_conf
-    menu_eq (0x5*9 + 0x3D), bank_numpot_values+9, process_change_conf
-    menu_eq (0x5*0xB + 0x3D), bank_numpot_values+0xA, process_change_conf
+    ;; menu_button edit_common_st_save, 0, tst_select_spec
+    ;; menu_edit edit_common_st_bank, 1, 1, 1, BANK_NB, current_bank, edit_common_load, UNUSED_PARAM
+    menu_edit 10, edit_common_st_bank, 1, 1, 1, BANK_NB, current_bank, tst_select_spec, UNUSED_PARAM
+    menu_edit_no_show 11, edit_common_st_save, 1, 2, 1, BANK_NB, current_bank, edit_common_refresh, edit_common_save
+    menu_eq 12, (0x5*0 + 0x3D), bank_numpot_values, process_change_conf
+    menu_eq 13, (0x5*1 + 0x3D), bank_numpot_values+1, process_change_conf
+    menu_eq 14, (0x5*2 + 0x3D), bank_numpot_values+2, process_change_conf
+    menu_eq 15, (0x5*3 + 0x3D), bank_numpot_values+3, process_change_conf
+    menu_eq 16, (0x5*4 + 0x3D), bank_numpot_values+4, process_change_conf
+    menu_eq 17, (0x5*5 + 0x3D), bank_numpot_values+5, process_change_conf
+    menu_eq 18, (0x5*6 + 0x3D), bank_numpot_values+6, process_change_conf
+    menu_eq 19, (0x5*7 + 0x3D), bank_numpot_values+7, process_change_conf
+    menu_eq 110, (0x5*8 + 0x3D), bank_numpot_values+8, process_change_conf
+    menu_eq 111, (0x5*9 + 0x3D), bank_numpot_values+9, process_change_conf
+    menu_eq 112,(0x5*0xB + 0x3D), bank_numpot_values+0xA, process_change_conf
     menu_end
 
     return
