fab0bcac0044f2eb590d0b0ce67b28612d0b0498
Cedric Bregardis <cedric.bregardis@gmail.com>
2010-09-11 14:18:18 +0200

    Add licence header

--- src/common/io.asm
+++ src/common/io.asm
@@ -1,3 +1,22 @@
+;;;
+;;; Copyright 2010 Cedric Bregardis.
+;;;
+;;; This file is part of EQ-lips firmware.
+;;;
+;;; EQ-lips firmware is free software: you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License as
+;;; published by the Free Software Foundation, version 3 of the
+;;; License.
+;;;
+;;; EQ-lips firmware is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with EQ-lips firmware.  If not, see <http://www.gnu.org/licenses/>.
+;;;
+
 #define IO_M
 #include <cpu.inc>
 #include <io.inc>
